d0d4482b2d427a00956da08c4dc2fa16b4d1adce
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-12 16:01:11 -0600

    Created basic Storage protocol

--- Sources/Storage/Storage.swift
+++ Sources/Storage/Storage.swift
@@ -1,3 +1,6 @@
-struct Storage {
-    var text = "Hello, World!"
+import Vapor
+
+public protocol Storage {
+    func store(file: File) -> Future<String>
+    func fetch(file: String) -> Future<File>
 }
