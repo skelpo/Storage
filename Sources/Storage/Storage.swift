eb6e858c76c01d157fa95e040c855339299b79fc
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-12 16:27:13 -0600

    Added 'path' parameter to Storage.store(file:at:) method

--- Sources/Storage/Storage.swift
+++ Sources/Storage/Storage.swift
@@ -1,6 +1,6 @@
 import Vapor
 
 public protocol Storage {
-    func store(file: File) -> Future<String>
+    func store(file: File, at path: String?) -> Future<String>
     func fetch(file: String) -> Future<File>
 }
