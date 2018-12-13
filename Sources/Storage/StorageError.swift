1d3c3718ee35b219f6c77f24d1b63238b002417e
Caleb Kleveter <caleb.kleveter@gmail.com>
2018-12-13 06:36:13 -0600

    Documented StorageError struct

--- Sources/Storage/StorageError.swift
+++ Sources/Storage/StorageError.swift
@@ -1,9 +1,19 @@
 import Core
 
+/// The error type that is thrown when a storage operation fails.
 public struct StorageError: Debuggable {
+    
+    /// A unique, machine readable, identifier for this error.
     public var identifier: String
+    
+    /// A human readable message that describes the reason for the error.
     public var reason: String
     
+    /// Creates a new `StorageError` instance.
+    ///
+    /// - Parameters:
+    ///   - identifier: A unique identifier for this error.
+    ///   - reason: A message that describes the reason for the error.
     public init(identifier: String, reason: String) {
         self.identifier = identifier
         self.reason = reason
