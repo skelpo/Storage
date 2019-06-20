cd8a030a59950076a434ea99a34b02c4b73d0128
Caleb Kleveter <caleb.kleveter@gmail.com>
2019-06-20 16:29:45 -0500

    Updated Vapor version to 4 alpha

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .library(name: "Storage", targets: ["Storage"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.1.0")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-alpha")
     ],
     targets: [
         .target(name: "Storage", dependencies: ["Vapor"]),
